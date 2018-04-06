ec2c6247bdf436785f20a5aa2081ce4a7bd62771
plmercereau <24897252+plmercereau@users.noreply.github.com>
2018-04-06 17:29:10 +0200

    https

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
-
-let app = express();
-app.use(history());
-app.use(serveStatic(__dirname + "/dist"));
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Listening on port ' + port)
-});
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+// create the express app
+const app = express()
+// create middleware to handle the serving the app
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+// Create default port to serve the app on
+// Import the package
+const secure = require('express-force-https');
+// Add it as a layer to the middleware
+app.use(secure)
+const port = process.env.PORT || 5000
+app.listen(port)
+// Log to feedback that this is actually running
+console.log('Server started on port ' + port)
