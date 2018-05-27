69b43621a1be1ed0e0f8c501cc0b115ae88e157b
plmercereau <24897252+plmercereau@users.noreply.github.com>
2018-05-27 21:26:13 +0200

    first generic excel export

--- src/components/ItemCollection.vue
+++ src/components/ItemCollection.vue
@@ -16,6 +16,7 @@
 <script>
   import * as firebase from 'firebase'
   import _ from 'lodash'
+  import XLSX from 'xlsx'
   import {addComputedValues, filterCollection, sortCollection} from '../schemas'
   // TODO create FAB button visible only under certain conditions described in the schema file
   export default {
@@ -33,11 +34,79 @@
         let template = this.schema.collectionView.default.subtitle || ''
         return _.template(template)(item)
       },
-      excelExport () { // TODO
-        let fields = this.schema.collectionView.default.export || Object.keys(this.schema.properties)
-        fields.forEach(fieldName => {
-          console.log(fieldName)
+      denormalizeDocument (sheets, schema, doc, recursive = true) {
+        // TODO forget about recursive param and test if doc already present in 'sheets'?
+        // TODO in this case (and maybe in any case) test if the firestore document is loaded already (i.e. load doc instead of ref)
+        if (!doc || !schema || !sheets) return
+        let fields = schema.collectionView.default.export || Object.keys(schema.properties)
+        if (!sheets[schema.name] || !sheets[schema.name].data) {
+          sheets[schema.name] = {
+            data: {},
+            headers: this.denormalizeHeaders(schema)
+          }
+        }
+        let obj = {}
+        fields.filter(field => doc[field]).forEach(field => {
+          let property = schema.properties[field]
+          switch (property.type) {
+            case 'location':
+              obj[field + '_latitude'] = doc[field].latitude
+              obj[field + '_longitude'] = doc[field].longitude
+              obj[schema.properties[field].zoom] = doc[schema.properties[field].zoom]
+              break
+            case 'collection':
+              if (property.unique) {
+                if (property.schema && recursive) { // TODO change scenario according to param
+                  this.denormalizeDocument(sheets, property.schema, doc[field], false)
+                }
+                obj[field] = doc[field].id || doc[field].value || doc[field]
+              } else if (property.schema && recursive) {
+                Object.keys(doc[field]).forEach(childDoc => {
+                  this.denormalizeDocument(sheets, property.schema, doc[field][childDoc], false)
+                })
+              }
+              break
+            default:
+              obj[field] = doc[field]
+              break
+          }
         })
+        obj.id = doc.id || doc.value || doc
+        sheets[schema.name].data[obj.id] = obj
+      },
+      denormalizeHeaders (schema) {
+        return this.exportFields(schema).reduce((obj, fieldName) => {
+          if (schema.properties[fieldName].type === 'collection') {
+            if (schema.properties[fieldName].unique) {
+              obj.push(fieldName)
+            }
+          } else if (schema.properties[fieldName].type === 'location') {
+            obj.push(fieldName + '_latitude')
+            obj.push(fieldName + '_longitude')
+            obj.push(schema.properties[fieldName].zoom)
+          } else {
+            obj.push(fieldName)
+          }
+          return obj
+        }, ['id'])
+      },
+      exportFields (schema) {
+        return schema.collectionView.default.export || Object.keys(schema.properties)
+      },
+      excelExport () { // TODO put some parameters
+        let sheets = {}
+        let arrayCollection = (_.isArray(this.collection)) ? this.collection : Object.keys(this.collection).map(name => {
+          return {id: name, ...this.collection[name]}
+        })
+        arrayCollection.forEach(doc => {
+          this.denormalizeDocument(sheets, this.schema, doc)
+        })
+        const wb = XLSX.utils.book_new()
+        Object.keys(sheets).forEach(name => {
+          const ws = XLSX.utils.json_to_sheet(Object.keys(sheets[name].data).map(id => sheets[name].data[id]), {header: sheets[name].headers})
+          XLSX.utils.book_append_sheet(wb, ws, name)
+        })
+        XLSX.writeFile(wb, `${this.schema.name}.xlsx`)
       }
     },
     computed: {
