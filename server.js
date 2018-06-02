b5ccc019ed83c21250ab6010fe57497fabbaa9d6
plmercereau <24897252+plmercereau@users.noreply.github.com>
2018-06-02 06:54:16 +0200

    labels 'application' changed into 'data tool'

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 // create the express app
-const app = express()
+const app = express();
 // create middleware to handle the serving the app
-app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) );
 // Catch all routes and redirect to the index file
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/dist/index.html')
-})
+});
 // Create default port to serve the app on
 // Import the package
 const secure = require('express-force-https');
 // Add it as a layer to the middleware
-app.use(secure)
-const port = process.env.PORT || 5000
-app.listen(port)
+app.use(secure);
+const port = process.env.PORT || 5000;
+app.listen(port);
 // Log to feedback that this is actually running
-console.log('Server started on port ' + port)
+console.log('Server started on port ' + port);
